71f76ac6a9e9e08dde97ef4d2c365eecda39a07f
muhanad-al-musawi <al-musawi-muhanad@hotmail.com>
2022-01-20 13:11:30 +0100

    Total price added

--- src/components/CustomerElement.jsx
+++ src/components/CustomerElement.jsx
@@ -13,12 +13,19 @@ function CustomerElement(props) {
   const [showMoreOrderInfo, setShowMoreOrderInfo] = useState(false)  
   
   return (
-     <div className='itemBox'> 
+     <div className='itemBox'>  
+     <div className='infoFlexEnd'>
+        <div className='moreOrderinfo' onClick={() => setShowMoreOrderInfo(true)}>i</div></div>   
+
+
+        <div className='orderFirstInfo'>
      <div className='orderFrom'>Your order from <span className='orderDate'>{orderDate.getDate()} .{orderDate.getMonth() + 1}.{orderDate.getFullYear()} </span> </div> 
-     <div>  Total Price {totalprice.toFixed(2).replace('.', ',')}€</div>
+     <div className='totalPrice'>  Total Price {totalprice.toFixed(2).replace('.', ',')}€</div> 
+     </div> 
+
+
       <div >  
-        <div className='infoFlexEnd'>
-        <div className='moreOrderinfo' onClick={() => setShowMoreOrderInfo(true)}>i</div></div>  
+        
 
         { showMoreOrderInfo &&      
       <div> 
